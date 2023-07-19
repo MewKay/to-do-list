ff2a9e3ca4714d849ae254d550a6631e5c4bb096
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-20 00:43:16 +0300

    Add code inside index.js for debugging

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ import "./style.css";
 import { ToDo } from "./todo";
 import { Project } from "./project";
 
+const myProject = Project();
+
+myProject.addToDo(
+  ToDo("Get Up"),
+  ToDo("Eat breakfast"),
+  ToDo("Eat Bread"),
+  ToDo("Dance yeah, i know to code wooo!")
+  );
+
+myProject.list.forEach(toDo => console.log(toDo.title));
+
+myProject.removeToDoWithTitle("Get Up","Eat breakfast");
+
+myProject.list.forEach(toDo => console.log(toDo.title));
