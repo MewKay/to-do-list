ed118a8019c414d1cd9fdfd2cdd54c89f5bef52e
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-19 23:29:05 +0300

    Create To-Do object

--- src/todo.js
+++ src/todo.js
@@ -1,6 +1,33 @@
-const ToDo = () => {
-  let title;
-  let description;
-  let dueDate;
-  let priority;
+const ToDo = (title) => {
+  let _description;
+  let _dueDate;
+  let _priority;
+ 
+  return {
+    get title() {
+      return title;
+    },
+    get description() {
+      return _description;
+    },
+    set description(value) {
+      _description = value;
+    },
+    get dueDate() {
+      return _dueDate;
+    },
+    set dueDate(value) {
+      _dueDate = value;
+    },
+    get priority() {
+      return _priority;
+    },
+    set priority(value) {
+      _priority = value;
+    }
+  }
 }
+
+export {
+  ToDo
+};
