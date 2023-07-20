1bb1e92c716d083fb4f4fd7b10a46e9b001c2021
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-21 01:05:39 +0300

    Add title to the header

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -1,6 +1,12 @@
+const createAppTitle = () => {
+  const appTitle = document.createElement("h1");
+  appTitle.innerText = "My To-do list";
+  return appTitle;
+}
+
 const Header = () => {
   const container = document.createElement("header");
-
+  container.appendChild(createAppTitle());
   return container;
 }
 
