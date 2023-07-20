48b308c407055426aa039a1ba97300e64ab24b94
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-20 19:01:22 +0300

    Delete test code inside index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,2 @@
 import "./reset.css";
 import "./style.css";
-import { ToDo } from "./todo";
-import { Project } from "./project";
-import { Priority } from "./priority";
-
-const myProject = Project();
-
-myProject.addToDo(
-  ToDo("Get Up"),
-  ToDo("Eat breakfast"),
-  ToDo("Eat Bread"),
-  ToDo("Dance yeah, i know to code wooo!")
-);
-
-myProject.list[2].toggleCompletionStatus();
-
-myProject.list.forEach( element => console.log(element.completionCheck))
