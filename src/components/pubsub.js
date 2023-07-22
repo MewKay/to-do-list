d9e1e32ea9d077070fd5730b1e3931189c069dfb
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-22 20:09:36 +0300

    Implement pubSub pattern

---
+++ src/components/pubsub.js
@@ -0,0 +1,34 @@
+const createPubSub = () => {
+  let events = {};
+
+  const subscribe = (eventName, functionToCall) => {
+    events[eventName] = events[eventName] || [];
+    events[eventName].push(functionToCall)
+  }
+
+  const unsubscribe = (eventName, functionToCall) => {
+    if(!events[eventName])
+      return;
+
+    events[eventName] = events[eventName].filter(functionToDelete => functionToDelete !== functionToCall);
+  }
+
+  const publish = (eventName, data) => {
+    if(!events[eventName])
+      return;
+
+    events[eventName].forEach(functionToCall => { functionToCall(data) });
+  }
+
+  return {
+    subscribe,
+    unsubscribe,
+    publish
+  };
+}
+
+const pubSub = createPubSub();
+
+export {
+  pubSub
+}
