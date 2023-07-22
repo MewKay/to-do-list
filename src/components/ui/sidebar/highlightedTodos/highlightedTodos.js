6efa80ca5b3f455efc1588ec4b6c5c9e54771c34
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-22 14:42:57 +0300

    Append item list to highlighted todos

--- src/components/ui/sidebar/highlightedTodos/highlightedTodos.js
+++ src/components/ui/sidebar/highlightedTodos/highlightedTodos.js
@@ -1,6 +1,14 @@
+import { itemImportant } from "./important";
+import { itemThisWeek } from "./thisWeek";
+import { itemToday } from "./today";
+
 const createList = () => {
   const list = document.createElement("ul");
   list.id = "list";
+
+  list.appendChild(itemToday);
+  list.appendChild(itemThisWeek);
+  list.appendChild(itemImportant);
 
   return list;
 }
