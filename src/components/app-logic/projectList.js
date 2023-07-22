8ec32c16847479ec70f501fb945b7ee51ed32020
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-22 18:36:58 +0300

    Create projectList handler

---
+++ src/components/app-logic/projectList.js
@@ -0,0 +1,25 @@
+const ProjectList = () => {
+  let _list = [];
+  
+  const addProject = (...Project) => {
+    _list.push(...Project);
+  }
+
+  const removeProjectWithName = (...ProjectName) => {
+    ProjectName.forEach( name => {
+      _list = _list.filter( project => project.name !== name);
+    });
+  } 
+
+  return {
+    get list() {
+      return _list;
+    },
+    addProject,
+    removeProjectWithName
+  };
+}
+
+export {
+  ProjectList
+};
