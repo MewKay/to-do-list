ccfece7fd2e0da09ae2add3477e247db7c87d5f5
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-25 01:10:06 +0300

    Fix task to mark as checked when changing content

--- src/components/ui/content/itemToDo/itemToDo.js
+++ src/components/ui/content/itemToDo/itemToDo.js
@@ -6,6 +6,7 @@ import { createContainer } from "../../createContainer";
 const createCheckBox = (toDo) => {
   const checkbox = createContainer("input");
   checkbox.type = "checkbox";
+  checkbox.checked = toDo.completionCheck;
   checkbox.addEventListener("change", () => {
     toDo.toggleCompletionStatus();
     pubSub.publish(Events.TO_DO_ITEM_CONTENT_UPDATE);
