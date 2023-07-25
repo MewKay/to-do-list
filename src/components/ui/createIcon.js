13d3c760863da08eb8aa3ed0008a8a22b0e6348b
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-26 01:44:06 +0300

    Prepare to add icons

---
+++ src/components/ui/createIcon.js
@@ -0,0 +1,12 @@
+import { createContainer } from "./createContainer"
+
+const createIcon = (iconName) => {
+  const icon = createContainer("i");
+  icon.classList.add("material-symbols-sharp");
+  icon.innerText = iconName;
+  return icon;
+}
+
+export {
+  createIcon
+}
