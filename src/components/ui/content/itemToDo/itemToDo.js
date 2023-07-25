a68079433ba1fdda35921d9e57c600c9d995bc5d
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-25 16:51:45 +0300

    Create button sections

--- src/components/ui/content/itemToDo/itemToDo.js
+++ src/components/ui/content/itemToDo/itemToDo.js
@@ -26,21 +26,72 @@ const createDueDateDisplay = (date) => {
   return dueDate;
 }
 
+const renderButtonSection = (buttons) => {
+  const section = createContainer("div");
+  section.classList.add("button-section");
+  for (const key in buttons) {
+    section.appendChild(buttons[key]()); 
+  }
+  return section;
+}
+
+const defaultTaskButtons = {
+  details: () => {
+    const detailsButton = createContainer("button");
+    detailsButton.innerText = "Details";
+    return detailsButton;
+  },
+  edit: () => {
+    const editButton = createContainer("button");
+    editButton.innerText = "Edit";
+    return editButton;
+  },
+  delete: () => {
+    const deleteButton = createContainer("button");
+    deleteButton.innerText = "Delete";
+    return deleteButton;
+  },
+}
+
+const confirmationDeleteButtons = {
+  yes: () => {
+    const yesButton = createContainer("button");
+    yesButton.innerText = "Yes";
+    return yesButton;
+  },
+  no: () => {
+    const noButton = createContainer("button");
+    noButton.innerText = "No";
+    return noButton;  
+  }
+}
+
+const {edit, ...taskCheckedButtons} = defaultTaskButtons;
+
 const ItemToDo = (toDo) => {
   const _container = createContainer("li");
   const _checkbox = createCheckBox(toDo);
   const _label = createToDoLabel(toDo.title);
   const _date = createDueDateDisplay(toDo.dueDate);
+  let buttonSection;
 
   const renderItem = () => {
-    if(toDo.completionCheck)
-      _container.classList.add("done");
-    if(!toDo.completionCheck)
-      _container.classList.remove("done"); 
-
+    _container.innerText = "";
     _container.appendChild(_checkbox);
     _container.appendChild(_label);
     _container.appendChild(_date);
+
+
+    if(toDo.completionCheck) {
+      _container.classList.add("done");
+      buttonSection = renderButtonSection(taskCheckedButtons);
+    }
+    if(!toDo.completionCheck) {
+      _container.classList.remove("done"); 
+      buttonSection = renderButtonSection(defaultTaskButtons);  
+    }
+
+    _container.appendChild(buttonSection);
   }
 
   renderItem();
