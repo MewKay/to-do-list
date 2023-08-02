ee5abee810872e935df6e1aba46af14e48622f43
MewKay <125735249+MewKay@users.noreply.github.com>
2023-08-02 18:08:52 +0300

    Show modal on click edit button

--- src/components/ui/content/itemToDo/primaryDisplay/buttonSection/editButton.js
+++ src/components/ui/content/itemToDo/primaryDisplay/buttonSection/editButton.js
@@ -1,8 +1,15 @@
 import { createContainer } from "../../../../createContainer";
 import { createIcon } from "../../../../createIcon";
+import { createModal } from "../../../../toDoForm/toDoForm";
 
 const createEditButton = () => {
   const editButton = createContainer("button", createIcon("edit"));
+  editButton.classList.add("btn-edit");
+  editButton.addEventListener("click", () => {
+    const modal = createModal();
+    document.body.appendChild(modal);
+    modal.showModal();
+  })
   return editButton;
 }
 
